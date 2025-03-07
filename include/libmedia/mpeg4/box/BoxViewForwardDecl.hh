e6ad657a7625cb08fe462b10ba0fc5f5c2cbcabe
Pavel Korotkevich <pawday@mail.ru>
2025-03-07 20:31:45 +0300

    Add forward declaration for all BoxViews

---
+++ include/libmedia/mpeg4/box/BoxViewForwardDecl.hh
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Mpeg4 {
+struct ChunkOffset64BoxView;
+struct ChunkOffsetBoxView;
+struct ChunkOffsetBoxView;
+struct FileTypeBoxView;
+struct ForwardDecl;
+struct HandlerBoxView;
+struct MediaHeaderBoxView;
+struct MovieHeaderBoxView;
+struct SampleDescriptionBoxView;
+struct SampleEntryBoxView;
+struct SampleSizeBoxView;
+struct TrackHeaderBoxView;
+} // namespace Mpeg4
